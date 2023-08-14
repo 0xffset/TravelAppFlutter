0d4fc936642e4a0d30bc4994899c81808c3928b1
rolEYder <roleyder02@gmail.com>
2023-08-13 20:49:10 -0400

    Rebuild

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,9 @@
-import 'dart:async';
 
-import 'package:custom_info_window/custom_info_window.dart';
-import 'package:travel_app/model/map_style.dart';
 import 'package:travel_app/pages/auth/auth_page.dart';
-import 'package:travel_app/pages/find_places.dart';
-import 'package:travel_app/pages/auth/login_page.dart';
 import 'package:travel_app/pages/home_page.dart';
 import 'package:travel_app/pages/main_page.dart';
-import 'package:travel_app/pages/map_circles.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:travel_app/pages/place_details_page.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
